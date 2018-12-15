b3c87e961b597bf8705388df86370b7514bbc5ca
Stephen Stewart <stephen.stewart@carisenda.com>
2018-12-15 22:09:07 +0000

    fixes

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,11 @@
+const nconf = require('./nconf');
+
 module.exports = {
 
   development: {
     debug: true,
     client: 'pg',
-    connection: 'postgres://localhost:5432/spa_development',
+    connection: nconf.get('db:connection'),
     migrations: {
       directory: __dirname + '/db/migrations'
     },
