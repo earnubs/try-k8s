67705f9261cd102641dc530dd9bf92ae441be72c
Stephen Stewart <stephen.stewart@carisenda.com>
2019-01-25 10:35:11 +0000

    Blender FTW (#2)

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 class Hello extends React.Component {
   render() {
     return (
-      <div>
-        Hello {this.props.name}
-      </div>
+      <App />
     );
   }
 }
